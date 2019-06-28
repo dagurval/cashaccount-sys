4fe42c43fab52318054c6dd15ac3cdde3e747580
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2019-06-28 10:09:42 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust bindings for libcashaccount
+
+Bindings for [libcashaccount](https://github.com/dagurval/libcashaccount)
---
+++ build.rs
@@ -0,0 +1,53 @@
+extern crate bindgen;
+extern crate cc;
+
+use std::env;
+use std::path::PathBuf;
+use std::fs;
+
+fn fail_on_empty_directory(name: &str) {
+    if fs::read_dir(name).unwrap().count() == 0 {
+        println!(
+                "The `{}` directory is empty, did you forget to pull the submodules?",
+                name
+                );
+        println!("Try `git submodule update --init --recursive`");
+        panic!();
+    }
+}
+
+fn compile_libcashaccount() {
+    let mut compiler = cc::Build::new();
+    compiler.opt_level(3);
+    compiler.include("libcashaccount/includes/");
+    compiler.include("libcashaccount/src/");
+
+    let source_files = vec!["libcashaccount/src/cashaccount.cpp"];
+
+    for f in source_files {
+        compiler.file(f);
+    }
+    compiler.cpp(true);
+    compiler.compile("libcashaccount.a");
+}
+
+fn main() {
+    println!("cargo:rustc-link-lib=static=cashaccount");
+    //println!("cargo:rustc-link-lib=cashaccount");
+    println!("cargo:rustc-link-search=native=libcashaccount/src");
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=libcashaccount/");
+
+    fail_on_empty_directory("libcashaccount");
+    compile_libcashaccount();
+
+    let bindings = bindgen::Builder::default()
+        .header("libcashaccount/includes/cashaccount.hpp")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
